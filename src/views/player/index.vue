e84b54769e2c3812e40045d3b7d27a4c1c1e7f85
jarhson.ning <jarhson.ning@huolala.cn>
2020-07-02 19:51:32 +0800

    完成初始化布局

---
+++ src/views/player/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="player-home">
+    <div class="body">
+      <div class="app">
+        <phone></phone>
+      </div>
+      <div class="info"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import phone from './components/pnone'
+  export default {
+    name: 'playerHome',
+    components: {
+      phone
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .player-home {
+    height: 100vh;
+    width: 100vw;
+    .body {
+      display: flex;
+      height: 100%;
+      width: 100%;
+
+      .app {
+        flex: 0 0 350px;
+      }
+      .info {
+        flex: 1;
+      }
+    }
+  }
+</style>
