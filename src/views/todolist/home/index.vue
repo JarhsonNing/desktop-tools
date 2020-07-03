9ae8d9deb11eeaff8e85481e4866e06bd5a5926a
YiField <sybilc@qq.com>
2020-07-03 14:48:14 +0800

    todolist页面

---
+++ src/views/todolist/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="text" v-model="inputValue">
+        <button @click="addItem">增加</button>
+        <ul>
+            <li v-for="(item, index) in list" :key="index">{{item}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            list: [],
+            inputValue: ''
+        }
+    },
+    methods:{
+        addItem(){
+            this.list.push(this.inputValue);
+            this.inputValue = ''
+        }
+    }
+}
+</script>
