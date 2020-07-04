5ed2142c86633de9ec31e63cb1157686e0fd307b
jarhson.ning <jarhson.ning@huolala.cn>
2020-07-04 11:47:05 +0800

    引入fontawesome字体图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <router-link to="/player/home">player</router-link>
+    <router-link to="/player/neteasy">player</router-link>
   </div>
 </template>
 
---
+++ src/views/player/neteasy/search/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="neteasy-search"></div>
+</template>
+<script>
+  export default {
+    name: 'playerNeteasySearch'
+  }
+</script>
