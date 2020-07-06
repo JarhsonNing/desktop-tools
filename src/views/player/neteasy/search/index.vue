37a5b34822fe4c7094c477f376ec5f5bc5824841
jarhson.ning <jarhson.ning@huolala.cn>
2020-07-06 10:38:28 +0800

    完成网易云的搜索页面

--- src/views/player/neteasy/search/index.vue
+++ src/views/player/neteasy/search/index.vue
@@ -1,8 +1,60 @@
 <template>
-  <div class="neteasy-search"></div>
+  <div class="neteasy-search">
+    <div class="header">
+      <md-toolbar class="md-accent">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <md-button class="md-icon-button" @click="$router.go(-1)">
+              <i class="fa fa-arrow-left"></i>
+            </md-button>
+          </div>
+
+          <md-autocomplete
+            v-model="searchValue"
+            :md-options="suggest"
+            @md-changed="handleValueChanged"
+          >
+            <label><i class="fa fa-search">搜索</i></label>
+            <template #md-autocomplete-item="{item}">
+              {{ item.name }}
+            </template>
+          </md-autocomplete>
+        </div>
+      </md-toolbar>
+      <md-progress-bar
+        v-if="showLoading"
+        class="md-accent"
+        md-mode="indeterminate"
+      ></md-progress-bar>
+    </div>
+  </div>
 </template>
 <script>
   export default {
-    name: 'playerNeteasySearch'
+    name: 'playerNeteasySearch',
+    data() {
+      return {
+        searchValue: '',
+        suggest: [],
+        showLoading: false
+      }
+    },
+    methods: {
+      handleValueChanged(val) {
+        console.log(val)
+        this.suggest = [{ name: val }]
+        this.countries = new Promise(resolve => {
+          setTimeout(() => {
+            if (!searchTerm) {
+              resolve(this.countryList)
+            } else {
+              const term = searchTerm.toLowerCase()
+
+              resolve(this.countryList.filter(({ name }) => name.toLowerCase().includes(term)))
+            }
+          }, 500)
+        })
+      }
+    }
   }
 </script>
