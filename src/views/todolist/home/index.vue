5b34792f74996e1e7ed5e56780a20f659755c4ff
YiField <sybilc@qq.com>
2020-07-06 09:47:38 +0800

    更新

--- src/views/todolist/home/index.vue
+++ src/views/todolist/home/index.vue
@@ -1,23 +1,32 @@
 <template>
     <div>
-        <input type="text" v-model="inputValue" placeholder="接下来做什么">
-        <button @click="addItem">增加</button>
-        <h2>待办</h2>
+        <input type="text" v-model="inputValue" placeholder="接下来做什么" @keyup.enter="addItem">
+        <h2>待办
+            <span v-if="unfinishedList.length && unfinishedList.length !=0">{{unfinishedList.length}}条</span>
+        </h2>
         <ol>
-            <li v-for="(item, index) in unfinishedList" :key="index">{{item}}
+            <li v-for="(item, index) in unfinishedList" :key="index">
+                <input type="text" v-model="unfinishedList[index].value" 
+                :disabled="!unfinishedList[index].disabled" @keyup.enter="disabledItem(index)" @blur="disabledItem(index)">
                 <button @click="completeItem(index)">完成</button>
                 <button @click="removeItem(index)">丢弃</button>
+                <button @click="editItem(index)">修改</button>
             </li>
         </ol>
-        <h2>已完成</h2>
+        <h2>已完成
+            <span v-if="finishedList.length && finishedList.length != 0">
+                {{finishedList.length}}条
+                </span>
+        </h2>
         <ol>
-            <li v-for="(item, index) in finishedList" :key="index">{{item}}
+            <li v-for="(item, index) in finishedList" :key="index">{{item.value}}
                 <button @click="deleteItem(index)">删除</button>
             </li>
         </ol>
     </div>
 </template>
 <script>
+import Vue from 'vue'
 export default {
     data() {
         return {
@@ -28,11 +37,16 @@ export default {
     },
     methods:{
         addItem() {
-            this.unfinishedList.push(this.inputValue);
+            if(!this.inputValue)return;
+            this.unfinishedList.push(
+                {value:this.inputValue});
             this.inputValue = '';
         },
         completeItem(index) {
             let item = this.unfinishedList[index];
+            if(item.disabled){
+                item.disabled = false;
+            }
             this.unfinishedList.splice(index, 1);
             this.finishedList.push(item);
         },
@@ -43,6 +57,18 @@ export default {
         deleteItem(index) {
             let item = this.finishedList[index];
             this.finishedList.splice(index, 1);
+        },
+        editItem(index){
+            if(!this.unfinishedList[index].disabled){
+                Vue.set(this.unfinishedList[index],'disabled',true)
+            }else{
+                this.unfinishedList[index].disabled = true;
+            }
+        },
+        disabledItem(index) {
+            if(this.unfinishedList[index].disabled){
+                this.unfinishedList[index].disabled = false;
+            }
         }
     }
 }
