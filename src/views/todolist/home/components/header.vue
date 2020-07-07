0293c5d64225601c1512996ee1221445bd3d637c
YiField <sybilc@qq.com>
2020-07-07 18:01:00 +0800

    更新

---
+++ src/views/todolist/home/components/header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="header">
+        <h1>Todo</h1>
+    </div>
+</template>
+<style lang="scss" scoped>
+.header {
+    
+}
+</style>
--- src/views/todolist/home/index.vue
+++ src/views/todolist/home/index.vue
@@ -1,45 +1,76 @@
 <template>
     <div>
-        <input type="text" v-model="inputValue" placeholder="接下来做什么" @keyup.enter="addItem">
-        <h2>待办
-            <span v-if="unfinishedList.length && unfinishedList.length !=0">{{unfinishedList.length}}条</span>
-        </h2>
-        <ol>
-            <li v-for="(item, index) in unfinishedList" :key="index">
-                <input type="text" v-model="unfinishedList[index].value" 
-                :disabled="!unfinishedList[index].disabled" @keyup.enter="disabledItem(item)" @dblclick="dblclick" @blur="disabledItem(item)">
-                <button @click="completeItem(item)">完成</button>
-                <button @click="removeItem(item)">丢弃</button>
-                <button @click="editItem(item)">修改</button>
-            </li>
-        </ol>
-        <h2>已完成
-            <span v-if="finishedList.length && finishedList.length != 0">
-                {{finishedList.length}}条
-                </span>
-        </h2>
-        <ol>
-            <li v-for="(item, index) in finishedList" :key="index">{{item.value}}
-                <button @click="deleteItem(index)">删除</button>
-            </li>
-        </ol>
+         <todoHeader></todoHeader>
+        <div class="todolist-wrapper md-layout md-gutter md-alignment-top-center">
+            <div class="md-layout-item md-size-50">
+                <md-field>
+                    <label>接下来做什么</label>
+                    <md-input v-model="inputValue" @keyup.enter="addItem"></md-input>
+                </md-field>
+                <md-list v-if="unfinishedList.length && unfinishedList.length !=0">
+                    <md-subheader>待办事件</md-subheader>
+                    <md-list-item v-for="(item, index) in unfinishedList" :key="index">
+                        <span class="md-list-item-text">{{item.value}}</span>
+                        <md-checkbox v-model="item.isCompleted"></md-checkbox>
+                    </md-list-item>
+                </md-list>
+                <!-- <h2>待办
+                    <span v-if="unfinishedList.length && unfinishedList.length !=0">{{unfinishedList.length}}条</span>
+                </h2>
+                <ol>
+                    <li v-for="(item, index) in unfinishedList" :key="index">
+                        <input type="text" v-model="unfinishedList[index].value" 
+                        :disabled="!unfinishedList[index].disabled" @keyup.enter="disabledItem(item)" @dblclick="dblclick" @blur="disabledItem(item)">
+                        <button @click="completeItem(item)">完成</button>
+                        <button @click="removeItem(item)">丢弃</button>
+                        <button @click="editItem(item)">修改</button>
+                    </li>
+                </ol> -->
+                <md-list v-if="finishedList.length && finishedList.length != 0">
+                    <md-subheader>已完成</md-subheader>
+                    <md-list-item>
+                        
+                    </md-list-item>
+                </md-list>
+                <h2>已完成
+                    <span v-if="finishedList.length && finishedList.length != 0">
+                        {{finishedList.length}}条
+                        </span>
+                </h2>
+                <ol>
+                    <li v-for="(item, index) in finishedList" :key="index">{{item.value}}
+                        <button @click="deleteItem(index)">删除</button>
+                    </li>
+                </ol>            
+            </div>
+    </div>
     </div>
 </template>
 <script>
 import Vue from 'vue'
+import todoHeader from './components/header'
 export default {
+    name: 'todolisthome',
     data() {
         return {
+            lists:[],
             unfinishedList: [],
             finishedList: [],
-            inputValue: ''
+            inputValue: '',
+            checkbox:'',
+            inline:''
         }
+    },
+    computed:{
+        // unfinishedList
     },
     methods:{
         addItem() {
             if(!this.inputValue.trim())return;
-            this.unfinishedList.push(
-                {value:this.inputValue});
+            this.unfinishedList.unshift({
+                value: this.inputValue,
+                isCompleted: false
+            })
             this.inputValue = '';
         },
         completeItem(item) {
@@ -73,6 +104,14 @@ export default {
         dblclick(){
             console.log('dblclick')
         }
+    },
+    components:{
+        todoHeader
     }
 }
 </script>
+<style lang="scss" scoped>
+.todolist-wrapper{
+    margin-top: 60px;
+}
+</style>
