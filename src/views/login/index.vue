a422e98143f8be4327c0ee73cd329b842f541842
YiField <sybilc@qq.com>
2020-07-29 18:08:51 +0800

    signup

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
-    <h2>Login</h2>
-    <div class="info-wrapper">
+    <h2>{{isloginPage?'登陆':'注册'}}</h2>
+    <div class="login-info-wrapper" v-show="isloginPage">
       <md-field>
         <label>用户名</label>
         <md-input v-model="userinfo.username"></md-input>
@@ -10,11 +10,25 @@
         <label>密码</label>
         <md-input v-model="userinfo.password"></md-input>
       </md-field>
-      <div class="btn-wrapper">
-        <md-button class="md-raised md-primary" style="margin-top:20px" @click="tosignup">登陆</md-button>
+    </div>
+    <div class="signup-info-wrapper" v-show="!isloginPage">
+      <md-field md-clearable>
+        <label>用户名</label>
+        <md-input v-model="userinfo.username"></md-input>
+      </md-field>
+      <md-field md-clearable>
+        <label>密码</label>
+        <md-input v-model="userinfo.password" type="password"></md-input>
+      </md-field>
+      <md-field :md-toggle-password="true">
+      <label>Password field without toggle</label>
+      <md-input v-model="userinfo.passwordC" type="password"></md-input>
+    </md-field>
+    </div>
+    <div class="btn-wrapper">
+        <md-button class="md-raised md-primary" style="margin-top:20px" @click="login">登陆</md-button>
         <md-button class="md-raised" style="margin-top:20px;margin-left:20px" @click="tosignup">去注册</md-button>        
       </div>
-    </div>
   </div>
 </template>
 
@@ -27,18 +41,20 @@
       return {
         userinfo: {
           username: '',
-          password: ''
-        }
+          password: '',
+          passwordC: ''
+        },
+        isloginPage:false
       }
     },
     mounted() {
 
     },
     methods: {
-    tosignup(){
-
-    },
-     async login(){
+      tosignup(){
+        this.isloginPage = false;
+      },
+      async login(){
         try {
           const { token } = await login(this.userinfo)
           localStorage.setItem('token', token)
@@ -48,7 +64,7 @@
           console.log(error)
         }
       }
-    },
+  },
 
 
   }
@@ -56,7 +72,7 @@
 <style lang="scss" scoped>
 .login {
   margin-top: 50px;
-  .info-wrapper {
+  .login-info-wrapper ,.signup-info-wrapper{
     width: 500px;
     margin:50px auto;
   }
