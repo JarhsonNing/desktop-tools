a771c90acb851b2ae2e0beb60d67d803b9aa8599
YiField <sybilc@qq.com>
2020-07-30 15:26:39 +0800

    back

--- src/api/login/index.js
+++ src/api/login/index.js
@@ -1,7 +1,5 @@
 import axios from '@/plugins/axios'
 
-const login = userinfo => {
-  console.log('login')
- return axios.post('login', userinfo)}
+const login = userinfo => axios.post('login', userinfo)
 const signup = userinfo => axios.post('user/register',userinfo)
 export { login,signup }
